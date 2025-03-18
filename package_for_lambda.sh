357c6e261935140c6b6a53f34f89476150f0f605
juandavid9611 <jd_rodrigueza@javeriana.edu.co>
2025-03-18 17:01:07 -0500

    Update lambda function

--- package_for_lambda.sh
+++ package_for_lambda.sh
@@ -3,9 +3,6 @@
 # Exit if any command fails
 set -eux pipefail
 
-# Clean up
-rm -rf lib
-
 pip install -t lib -r requirements.txt
 (cd lib; zip ../lambda_function.zip -r  .)
 zip lambda_function.zip -u app.py
@@ -13,6 +10,9 @@ zip lambda_function.zip -u cognito_idp_actions.py
 zip lambda_function.zip -u JWTBearer.py
 zip lambda_function.zip -u auth.py
 
+# Clean up
+rm -rf lib
+rm -rf lambda_function.zip
 
 git add .
 git commit -m "Update lambda function"
