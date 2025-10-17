884ee1fda2caf962d2cd453f4ff48bc017ec061f
juandavid9611 <jd_rodrigueza@javeriana.edu.co>
2025-10-17 13:09:01 -0500

    Package Lambda: nueva estructura (api/builders/repositories/services/utils)

--- utils/env_utils.py
+++ utils/env_utils.py
@@ -4,5 +4,5 @@ import os
 def _env() -> str:
     return os.environ.get("ENV", "dev")
 
-def _use_mangum():
-    return os.environ.get("USE_MANGUM", False)
+def _use_mangum() -> str:
+    return os.environ.get("USE_MANGUM", "false")
